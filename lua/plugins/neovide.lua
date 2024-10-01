258e1c661889c6b33107a7a946d4c994f6cfcd6e
Hide-v <vcmingri19@outlook.com>
2024-10-01 12:09:07 +0800

    Inital commit

---
+++ lua/plugins/neovide.lua
@@ -0,0 +1,30 @@
+if not vim.g.neovide then return {} end
+
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      opt = { -- configure vim.opt options
+        -- configure font
+        guifont = "JetBrainsMono Nerd Font:h10",
+        -- line spacing
+        linespace = 0,
+      },
+      g = { -- configure vim.g variables
+        -- configure scaling
+        neovide_scale_factor = 1.0,
+        -- configure padding
+        neovide_padding_top = 0,
+        neovide_padding_bottom = 0,
+        neovide_padding_right = 0,
+        neovide_padding_left = 0,
+        -- Animation effects
+        neovide_cursor_animation_length = 0.13,
+        neovide_cursor_trail_length = 0.8,
+        neovide_cursor_anitialiasing = true,
+        neovide_cursor_vfx_mode = "pixiedust", -- options "railgun、torpedo、pixiedust etc."
+      },
+    },
+  },
+}
